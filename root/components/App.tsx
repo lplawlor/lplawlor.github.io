42a41e895c6966e84656c06fb7b0f04cd79d4d30
lplawlor <lplawlor@mun.ca>
2023-04-24 19:05:47 -0230

    Added package for proper 100vh behaviour on mobile
    The viewport on Safari iOS includes the part of the page under the
    Safari navbar. The react-div-100vh package adds a Div100vh React
    component which fixes this issue.
    
    Also, I made the h3 tags center their text for thinner displays.

--- root/components/App.tsx
+++ root/components/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import LinkedInBadge from "./LinkedInBadge";
+import Div100vh from "react-div-100vh";
+import "../styles/App.css";
 
 function App() {
   return (
-    <>
+    <Div100vh id="app">
       <div className="d-flex flex-column align-items-center my-auto">
-        <h3>This site is under construction.</h3>
-        <h3 className="mb-3">
+        <h3 className="text-center">This site is under construction.</h3>
+        <h3 className="text-center mb-3">
           In the meantime, please visit my LinkedIn profile!
         </h3>
         <LinkedInBadge />
@@ -14,7 +16,7 @@ function App() {
       <footer className="ms-auto me-2 mt-auto mb-2 small">
         Made with React & Bootstrap
       </footer>
-    </>
+    </Div100vh>
   );
 }
 
