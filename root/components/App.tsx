4e997d394c972554b0eb3f904eaf313d3ecd30ce
lplawlor <lplawlor@mun.ca>
2023-04-26 16:01:48 -0230

    Added Resume (Apr26_2023_v2)

--- root/components/App.tsx
+++ root/components/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import LinkedInBadge from "./LinkedInBadge";
 import Div100vh from "react-div-100vh";
 import "../styles/App.css";
+import resume from "../files/LiamLawlorResume.pdf";
 
 function App() {
   return (
@@ -9,7 +10,11 @@ function App() {
       <div className="d-flex flex-column align-items-center my-auto">
         <h3 className="text-center">This site is under construction.</h3>
         <h3 className="text-center mb-3">
-          In the meantime, please visit my LinkedIn profile!
+          In the meantime, please visit my LinkedIn profile or view my&nbsp;
+          <a className="btn-link" href={resume} target="_blank">
+            résumé
+          </a>
+          !
         </h3>
         <LinkedInBadge />
       </div>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith("pdf")) {
+            // Don't hash pdf files
+            return "assets/[name][extname]";
+          } else {
+            // Hash everything else
+            return "assets/[name]-[hash][extname]";
+          }
+        },
+      },
+    },
+  },
 });
