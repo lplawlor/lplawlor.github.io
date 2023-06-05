9ee35d08f00e4b462e6454f8f04ccafbfc2c523b
lplawlor <lplawlor@mun.ca>
2023-06-05 14:22:09 -0230

    Setting Up Tailwind and Starting From Scratch

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,11 @@
-import LinkedInBadge from "./LinkedInBadge";
 import Div100vh from "react-div-100vh";
-import "../styles/App.css";
-import resume from "../files/LiamLawlorResume.pdf";
 
 function App() {
   return (
-    <Div100vh id="app" className="mx-3">
-      <div className="d-flex flex-column align-items-center my-auto">
-        <h3 className="text-center">This site is under construction.</h3>
-        <h3 className="text-center mb-3">
-          In the meantime, please visit my LinkedIn profile or view my&nbsp;
-          <a className="btn-link" href={resume} target="_blank">
-            résumé
-          </a>
-          !
-        </h3>
-        <LinkedInBadge />
-      </div>
-      <footer className="ms-auto me-2 mt-auto mb-2 small">
-        Made with React & Bootstrap
-      </footer>
+    <Div100vh>
+      <h1 className="text-3xl font-light underline">
+        Hello world!
+      </h1>
     </Div100vh>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/index.css"
 import App from "./components/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
